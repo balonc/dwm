d35fdaa8a7c2b6af92f615bfa212483e52230a96
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-10-29 21:36:24 +0100

    Actualización de autostart.sh con lanamiento de numlockx y unclutter

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+# Copy to ~/.config/dwm/autostart.sh
+
 xrdb ~/.Xresources
 xbindkeys
+unclutter
+numlockx
 urxvtd -q &
 while true; do
     xsetroot -name "Bat:$(acpi | awk '{print $4}' | tr -d \,)   $(date +"%H:%M")"
