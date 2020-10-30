7fdd372095975905986fae620fafacfaf506d735
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-10-30 15:07:01 +0100

    Solución de comandos en autorun.sh por falta de lanzamiento a segundo plano (&)

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-# Copy to ~/.config/dwm/autostart.sh
-
 xrdb ~/.Xresources
 xbindkeys
-unclutter
+unclutter &
 numlockx
+feh --bg-scale background.png
 urxvtd -q &
+dunst &
 while true; do
     xsetroot -name "Bat:$(acpi | awk '{print $4}' | tr -d \,)   $(date +"%H:%M")"
     sleep 2
