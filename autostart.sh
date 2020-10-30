1a401472ee8dfc9cf67d2573f931f6fa1d8487b7
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-10-30 17:28:03 +0100

    Añadidos control de brillo, volumen e información avanzada de batería a autorun

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,13 @@
-xrdb ~/.Xresources
-xbindkeys
+xrdb ~/.Xresources 
+xbindkeys 
 unclutter &
-numlockx
+numlockx 
 feh --bg-scale background.png
 urxvtd -q &
 dunst &
+
 while true; do
-    xsetroot -name "Bat:$(acpi | awk '{print $4}' | tr -d \,)   $(date +"%H:%M")"
+    #xsetroot -name "Bat:$(acpi | awk '{print $4}' | tr -d \,)   $(date +"%H:%M")"
+    xsetroot -name "Brightness:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m)   Volume:$(pamixer --get-volume-human)  $(acpi)  $(date +"%H:%M")"
     sleep 2
 done
