2b25f2a38d4bd131b410645c42b521d4a7c0abe9
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-11-05 00:05:43 +0100

    Añadido sistema de lock con xautolock & xsecurelock

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,7 @@ numlockx
 feh --bg-scale background.png
 urxvtd -q &
 dunst &
+xautolock -time 15 -notify 5 -notifier '/usr/lib/xsecurelock/until_nonidle /usr/lib/xsecurelock/dimmer' -locker xsecurelock &
 
 while true; do
     #xsetroot -name "Bat:$(acpi | awk '{print $4}' | tr -d \,)   $(date +"%H:%M")"
