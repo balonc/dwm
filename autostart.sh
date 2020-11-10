72e1119d2d88bbf0aec8ebb54217fb55fb51e89f
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-11-10 17:45:22 +0100

    Añadida notificación de batería crítica en autorun.sh por notify-send

--- autostart.sh
+++ autostart.sh
@@ -9,6 +9,13 @@ xautolock -time 15 -notify 5 -notifier '/usr/lib/xsecurelock/until_nonidle /usr/
 
 while true; do
     #xsetroot -name "Bat:$(acpi | awk '{print $4}' | tr -d \,)   $(date +"%H:%M")"
-    xsetroot -name "Brightness:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m)   Volume:$(pamixer --get-volume-human)  $(acpi)  $(date +"%H:%M")"
+    BATT=$( acpi -b | sed 's/.*[charging|unknown], \([0-9]*\)%.*/\1/gi' )
+    STATUS=$( acpi -b | sed 's/.*: \([a-zA-Z]*\),.*/\1/gi' )
+
+    if ([ $BATT -le 5] && [ $STATUS == 'Discharging' ]); then
+	notify-send --expire-time=5000 --urgency=critical "critical battery status. Now $($STATUS),$($BATT)%"
+    fi
+
+    xsetroot -name "Brightness:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m)   Volume:$(pamixer --get-volume-human)  Battery: $STATUS,$BATT%  $(date +"%H:%M")"
     sleep 2
 done
