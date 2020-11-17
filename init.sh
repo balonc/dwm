f88e18c726714a1654ed52dabedcc294ed0cbdbe
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-11-17 23:44:29 +0100

    Comentado puntualmente detectores de batería por mal funcionamiento

--- init.sh
+++ init.sh
@@ -12,15 +12,15 @@ while true; do
     BATT=$( acpi -b | sed 's/.*[charging|unknown], \([0-9]*\)%.*/\1/gi' )
     STATUS=$( acpi -b | sed 's/.*: \([a-zA-Z]*\),.*/\1/gi' )
 
-    if ([ $BATT -le 20] && [ $STATUS == 'Discharging' ]); then
-	notify-send --expire-time=5000 --urgency=normal "low [-20%] battery status. Now $($STATUS),$($BATT)%"
-    elif ([ $BATT -le 15] && [ $STATUS == 'Discharging' ]); then
-	 notify-send --expire-time=5000 --urgency=critical "critical [-15%] battery status. Now $($STATUS),$($BATT)%"
-    elif ([ $BATT -le 10] && [ $STATUS == 'Discharging']); then
-	 notify-send --expire-time=5000 --urgency=critical "critical [-10%] battery status. Now $($STATUS),$($BATT)%"
-    elif ([ $BATT -le 5] && [ $STATUS == 'Discharging']); then
-	 notify-send --expire-time=5000 --urgency=critical "critical [-5%] battery status. Now $($STATUS),$($BATT)%"
-    fi
+#    if ([ $BATT -le 20] && [ $STATUS == 'Discharging' ]); then
+#	notify-send --expire-time=5000 --urgency=normal "low [-20%] battery status. Now $($STATUS),$($BATT)%"
+#    elif ([ $BATT -le 15] && [ $STATUS == 'Discharging' ]); then
+#	 notify-send --expire-time=5000 --urgency=critical "critical [-15%] battery status. Now $($STATUS),$($BATT)%"
+#    elif ([ $BATT -le 10] && [ $STATUS == 'Discharging']); then
+#	 notify-send --expire-time=5000 --urgency=critical "critical [-10%] battery status. Now $($STATUS),$($BATT)%"
+#    elif ([ $BATT -le 5] && [ $STATUS == 'Discharging']); then
+#	 notify-send --expire-time=5000 --urgency=critical "critical [-5%] battery status. Now $($STATUS),$($BATT)%"
+#    fi
 
     xsetroot -name "Brightness:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m) Volume:$(pamixer --get-volume-human) Battery: $STATUS,$BATT%  $(date +"%H:%M")"
     sleep 2
