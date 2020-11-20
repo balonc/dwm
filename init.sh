78250e8d9803006bb7fa0b6f3b82e044b76d63fa
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-11-20 19:17:17 +0100

    Añadido a init.sh avisos de status2d con batería baja

--- init.sh
+++ init.sh
@@ -1,27 +1,23 @@
-xrdb ~/.Xresources 
-xbindkeys 
+xrdb ~/.Xresources
+xbindkeys
 unclutter &
-numlockx 
+numlockx
 feh --bg-scale background.png
 urxvtd -q &
 dunst &
 #xautolock -time 15 -notify 5 -notifier '/usr/lib/xsecurelock/until_nonidle /usr/lib/xsecurelock/dimmer' -locker xsecurelock &
 
 while true; do
-    #xsetroot -name "Bat:$(acpi | awk '{print $4}' | tr -d \,)   $(date +"%H:%M")"
     BATT=$( acpi -b | sed 's/.*[charging|unknown], \([0-9]*\)%.*/\1/gi' )
     STATUS=$( acpi -b | sed 's/.*: \([a-zA-Z]*\),.*/\1/gi' )
 
-#    if ([ $BATT -le 20] && [ $STATUS == 'Discharging' ]); then
-#	notify-send --expire-time=5000 --urgency=normal "low [-20%] battery status. Now $($STATUS),$($BATT)%"
-#    elif ([ $BATT -le 15] && [ $STATUS == 'Discharging' ]); then
-#	 notify-send --expire-time=5000 --urgency=critical "critical [-15%] battery status. Now $($STATUS),$($BATT)%"
-#    elif ([ $BATT -le 10] && [ $STATUS == 'Discharging']); then
-#	 notify-send --expire-time=5000 --urgency=critical "critical [-10%] battery status. Now $($STATUS),$($BATT)%"
-#    elif ([ $BATT -le 5] && [ $STATUS == 'Discharging']); then
-#	 notify-send --expire-time=5000 --urgency=critical "critical [-5%] battery status. Now $($STATUS),$($BATT)%"
-#    fi
+    if [ "${BATT}" -le 25 ] && [ "${STATUS}" == "Discharging" ]; then
+        xsetroot -name "b:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m), v:$(pamixer --get-volume-human), ^c#151515^^b#d2dd30^e:$STATUS,$BATT%^d^  $(date +"%H:%M")"
+    elif [ "${BATT}" -le 15 ] && [ "${STATUS}" == "Discharging" ]; then
+        xsetroot -name "b:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m), v:$(pamixer --get-volume-human), ^c#ffffff^^b#e5211d^e:$STATUS,$BATT%^d^  $(date +"%H:%M")"
+    else 
+        xstroot -name "b:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m), v:$(pamixer --get-volume-human), e:$STATUS,$BATT%  $(date +"%H:%M")"
+    fi
 
-    xsetroot -name "b:$(brightnessctl --device=intel_backlight g)/$(brightnessctl --device=intel_backlight m), v:$(pamixer --get-volume-human), e:$STATUS,$BATT%  $(date +"%H:%M")"
     sleep 2
 done
