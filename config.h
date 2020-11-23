2da30dacb12067566745056dd26226e5076e2647
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2020-11-23 11:35:58 +0100

    Actualización de paleta de colores y optimización de colores

--- config.h
+++ config.h
@@ -11,7 +11,8 @@ static const char col_gray2[]       = "#222222"; // "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_urxvt[]        = "#151515";
+static const char col_urxvt[]       = "#151515";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_urxvt, col_gray2 },
--- init.sh
+++ init.sh
@@ -7,6 +7,13 @@ urxvtd -q &
 dunst &
 #xautolock -time 15 -notify 5 -notifier '/usr/lib/xsecurelock/until_nonidle /usr/lib/xsecurelock/dimmer' -locker xsecurelock &
 
+CYAN="#005677"
+GREEN="#008A33"
+RED="#be2100"
+YELLOW="be8700"
+WHITE="#ffffff"
+BLACK="#151515"
+
 while true; do
     BATT=$( acpi -b | sed 's/.*[charging|unknown], \([0-9]*\)%.*/\1/gi' )
     STATUS=$( acpi -b | sed 's/.*: \([a-zA-Z]*\),.*/\1/gi' )
@@ -22,11 +29,13 @@ while true; do
     S_DATETIME="$DATETIME"
 
     if [ "${BATT}" -le 15 ] && [ "${STATUS}" == "Discharging" ]; then
-        S_BATTERY="^c#ffffff^^b#e5211d^ϟ:$STATUS,$BATT%^d^"
+        S_BATTERY="^c$WHITE^^b$RED^ϟ:$STATUS,$BATT%^d^"
     elif [ "${BATT}" -le 30 ] && [ "${STATUS}" == "Discharging" ]; then
-        S_BATTERY="^c#151515^^b#d2dd30^ϟ:$STATUS,$BATT%^d^"
-    elif [ "${BATT}" == 100 ] && [ "${STATUS}" == "Charging" ]; then
-        S_BATTERY="^c#151515^^b#1b9104^ϟ:$STATUS,$BATT%^d^"
+        S_BATTERY="^c$BLACK^^b$YELLOW^ϟ:$STATUS,$BATT%^d^"
+    elif [ "${STATUS}" == "Full" ]; then
+        S_BATTERY="^c$BLACK^^b$CYAN^ϟ:$STATUS^d^"
+    elif [ "${STATUS}" == "Charging" ]; then
+        S_BATTERY="^c$BLACK^^b$GREEN^ϟϟ:$STATUS,$BATT%^d^"
     fi
 
     xsetroot -name "$S_BRIGHTNESS, $S_VOLUME, $S_BATTERY, $S_DATETIME";
