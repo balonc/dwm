053afed7ab8198435fb16a43ffbd2f8bd29a5a4c
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2021-01-16 21:48:22 +0100

    Add dwm-titlecolor-20190206-6.2.diff

--- config.h
+++ config.h
@@ -17,6 +17,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTitle]  = { col_gray4, col_urxvt,  col_cyan  },
 };
 
 /* tagging */
@@ -35,7 +36,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
