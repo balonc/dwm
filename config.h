3e65acbf40d6ddf12f62fb80f60360172fd4f737
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2021-01-23 00:23:45 +0100

    Actualización de paleta de colores

--- config.h
+++ config.h
@@ -6,18 +6,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#151515"; // "#222222";
-static const char col_gray2[]       = "#222222"; // "#444444";
+static const char col_gray1[]       = "#000000"; // "#151515"; // "#222222";
+static const char col_gray2[]       = "#000000"; // "#222222"; // "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_urxvt[]       = "#151515";
+static const char col_urxvt[]       = "#000000";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeTitle]  = { col_gray4, col_urxvt,  col_cyan  },
+	[SchemeTitle]  = { col_gray3, col_gray1,  col_gray2  },
 };
 
 /* tagging */
